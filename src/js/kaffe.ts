56c4556cd4602d7fafe4206c9f7f5cf689512836
KimNowak47 <k2them3@gmail.com>
2019-03-20 13:17:05 +0100

    implementeret Kaffe klassen

---
+++ src/js/kaffe.ts
@@ -0,0 +1,22 @@
+
+
+
+
+abstract class Kaffe
+{
+    public Rabat:number;
+
+    abstract pris(): number;
+    
+    styrke(): string
+    {
+        return "stærk";
+    }
+
+    constructor() {
+        this.Rabat = 0;
+    }
+
+
+
+}
